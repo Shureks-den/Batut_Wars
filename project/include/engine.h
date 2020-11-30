a35c48fb4a715ae456468176b4298deca17733b0
Tony <64084206+muzhts-anton@users.noreply.github.com>
2020-12-01 00:32:04 +0300

    Update engine.h
    the last version of engine.h

--- project/include/engine.h
+++ project/include/engine.h
@@ -2,70 +2,69 @@
 
 #include <SFML/Graphics.hpp>
 
-#define SPEED_LIMIT 30.0 //  speed и acceleretion не могут быть выше, чем
+#define SPEED_LIMIT 30.0 // speed и acceleretion не могут быть выше, чем
 #define ACCELERETION_LIMIT 4.0
 
 enum class Direction {
-    FORWARD = 0, 
-    BACKWARD,
-    LEFT,
-    RIGHT  
+	FORWARD = 0,
+	BACKWARD,
+	LEFT,
+	RIGHT
 };
 
 namespace engine {
+	class Vector {
+	public:
+		Vector() = default;
+		Vector(double x, double y);
+		Vector(const Vector & other) = default;
+		~Vector() = default;
+		double get_x() const;
+		double get_y() const;
+		double set_x(double x);
+		double set_y(double y);
+		float get_abs() const;
+		void rotate(double angle);
+		Vector operator+(const Vector & other) const;
+		Vector operator-(const Vector & other) const;
+		Vector operator*(double coef) const;
+		Vector operator/(double coef) const;
+		Vector operator+=(const Vector & other);
+		Vector operator-=(const Vector & other);
+		Vector operator*=(double coef);
+		Vector operator/=(double coef);
 
-class Vector {
- public:
-    Vector() = default;
-    Vector(double x, double y);
-    Vector(const Vector &other) = default;
-    ~Vector() = default;
-    double get_x() const;
-    double get_y() const;
-    double set_x(double x);
-    double set_y(double y);
-    float get_abs() const;
-    void rotate(double angle);
-    Vector operator+(const Vector &other) const;
-    Vector operator-(const Vector &other) const;
-    Vector operator*(double coef) const;
-    Vector operator/(double coef) const;
-    Vector operator+=(const Vector &other);
-    Vector operator-=(const Vector &other);
-    Vector operator*=(double coef);
-    Vector operator/=(double coef);
+	private:
+		double _x;
+		double _y;
+	};
 
- private:
-    double _x;
-    double _y;
-};
+	class SpaceObject {
+	public:
+		SpaceObject() = default;
+		virtual ~SpaceObject();
+		float get_x() const;
+		float get_y() const;
+		float set_x(float x);
+		float set_y(float y);
+		virtual void update(sf::Time dt);
 
-class SpaceObject {
- public:
-    SpaceObject() = default;
-    virtual ~SpaceObject();
-    float get_x() const;
-    float get_y() const;
-    float set_x(float x);
-    float set_y(float y);
-    virtual void update(sf::Time dt);
+	protected:
+		sf::Vector2f _coordinate;
+	};
 
- protected:
-    sf::Vector2f _coordinate;
-};
+	class MoveAble : SpaceObject {
+	public:
+		MoveAble() = default;
+		virtual ~MoveAble();
+		virtual void update(sf::Time dt) = 0;
+		void rotate(float angle);
+		void give_acceleration(Vector acceleration);
+		Vector d_acceleration(Direction direction, float abs_, float d_acceleration);
+		void give_acceleration(Direction direction, float d_acceleration);
 
-class MoveAble : SpaceObject {
- public:
-    MoveAble() = default;
-    virtual ~MoveAble();
-    virtual void update(sf::Time dt) = 0;
-    void rotate(Direction direction, float angle);  // Повернуть вектор скорости на angle в сторону direction (FORWARD
-    // сонаправленно со speed_)
-    void give_acceleretion(Vector acceleration);  // Изменить ускорение на acceleration (сумма)
-    void give_acceleretion(Direction direction, float d_acceleration);  // Изменить ускорение на d_acceleration в сторону direction
-
- protected:
-    Vector _speed;
-    Vector _acceleretion;
-};
-
+	protected:
+		Vector _speed;
+		Vector _acceleration;
+	};
+}
