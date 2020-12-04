2048a1515ed2f7645c600a306767a667b0bc6499
Bra26ey <62746788+Bra26ey@users.noreply.github.com>
2020-12-04 03:06:10 +0300

    add actions

--- project/include/world.h
+++ project/include/world.h
@@ -2,6 +2,7 @@
 
 #include "engine.h"
 #include "player.h"
+#include "status.h"
 
 class World {
  public:
@@ -11,10 +12,12 @@ class World {
     void update(sf::Time d_time);
     void push_back(engine::Entity &object);
     std::queue<Action>& get_actions();
+    std::vector<Status>& get_status();
 
  private:
     std::vector<engine::Entity> _objects;
     std::queue<Action> _actions;
+    std::vector<Status> _status; 
 
     void do_action(size_t id, Action action, sf::Time d_time);
 };
