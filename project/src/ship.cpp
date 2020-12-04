1497697ea33ada1a0c8aa18074cb508d671711ce
Tony <64084206+muzhts-anton@users.noreply.github.com>
2020-12-04 20:07:19 +0300

    First update
    Андрей, мы так и не поняли до конца, что от нас хотят. В основном из-за того, что ни я, ни Лизочка не имеем полного представления о глобальной структуре проекта. по идее такой метод update должен быть в классе Game.

--- project/src/ship.cpp
+++ project/src/ship.cpp
@@ -1,3 +1,19 @@
 #include "ship.h"
 
-// TODO(ANDY) реализация методов класса Ship
+using namespace space;
+
+void space::Ship::update(sf::Time dt)
+{
+	_speed += _acceleration * dt.asSeconds();
+	sf::Vector2f movement(0.f, 0.f);
+	if (mIsMovingUp)
+		movement.y -= _speed.get_y();
+	if (mIsMovingDown)
+		movement.y += _speed.get_y();
+	if (mIsMovingLeft)
+		movement.x -= _speed.get_x();
+	if (mIsMovingRight)
+		movement.x += _speed.get_x();
+
+	mPlayer.move(movement * dt.asSeconds());
+}
