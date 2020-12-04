fefa041633226a97056fbe259bd6d486c7ca0866
Tony <64084206+muzhts-anton@users.noreply.github.com>
2020-12-04 20:04:11 +0300

    Working version of engine
    Код отформатирую позже.

--- project/include/engine.h
+++ project/include/engine.h
@@ -2,74 +2,79 @@
 
 #include <SFML/Graphics.hpp>
 
-#define SPEED_LIMIT 30.0  // speed и acceleretion не могут быть выше, чем
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define SPEED_LIMIT 30.0
 #define ACCELERETION_LIMIT 4.0
 
-enum class Direction {
+enum class Direction
+{
     FORWARD = 0,
     BACKWARD,
     LEFT,
     RIGHT
 };
 
-namespace engine {
+namespace engine
+{
 
-class Vector {
- public:
-    Vector() = default;
-    Vector(double x, double y);
-    Vector(const Vector & other) = default;
-    ~Vector() = default;
-    double get_x() const;
-    double get_y() const;
-    void set_x(double x);
-    void set_y(double y);
-    float get_abs() const;
-    void rotate(double angle);
-    Vector get_normal();
-    Vector operator+(const Vector & other) const;
-    Vector operator-(const Vector & other) const;
-    Vector operator*(double coef) const;
-    Vector operator/(double coef) const;
-    Vector operator+=(const Vector & other);
-    Vector operator-=(const Vector & other);
-    Vector operator*=(double coef);
-    Vector operator/=(double coef);
+    class Vector
+    {
+    public:
+        Vector() = default;
+        Vector(double x, double y);
+        Vector(const Vector & other);
+        ~Vector() = default;
+        double get_x() const;
+        double get_y() const;
+        void set_x(double x);
+        void set_y(double y);
+        float get_abs() const;
+        void rotate(double angle);
+        Vector get_normal();
+        Vector operator+(const Vector & other) const;
+        Vector operator-(const Vector & other) const;
+        Vector operator*(double coef) const;
+        Vector operator/(double coef) const;
+        Vector operator+=(const Vector & other);
+        Vector operator-=(const Vector & other);
+        Vector operator*=(double coef);
+        Vector operator/=(double coef);
 
- private:
-    double _x;
-    double _y;
-};
+    private:
+        double _x;
+        double _y;
+    };
 
-class Entity {
- public:
-    Entity() = default;
-    virtual ~Entity();
-    float get_x() const;
-    float get_y() const;
-    float set_x(float x);
-    float set_y(float y);
-    virtual void update(sf::Time dt);
+    class Entity
+    {
+    public:
+        Entity() = default;
+        virtual ~Entity();
+        float get_x() const;
+        float get_y() const;
+        float set_x(float x);
+        float set_y(float y);
+        virtual void update(sf::Time dt);
 
- protected:
-    sf::Vector2f _coordinate;
-};
+    protected:
+        sf::Vector2f _coordinate;
+    };
 
-class MoveAble : public Entity {
- public:
-    MoveAble() = default;
-    virtual ~MoveAble();
-    virtual void update(sf::Time dt) = 0;
-    void rotate(float angle);
-    void give_acceleration(Vector acceleration);
-    void give_acceleration(Direction direction, float d_acceleration);
+    class MoveAble : public Entity
+    {
+    public:
+        MoveAble() = default;
+        virtual ~MoveAble();
+        virtual void update(sf::Time dt) = 0;
+        void rotate(float angle);
+        void give_acceleration(Vector acceleration);
+        void give_acceleration(Direction direction, float d_acceleration);
 
- private:
-    Vector d_acceleration(Direction direction, float d_acceleration);
-
- protected:
-      Vector _speed;
-      Vector _acceleration;
-};
+    protected:
+        Vector _speed;
+        Vector _acceleration;
+    };
 
 }  // end namespace engine
