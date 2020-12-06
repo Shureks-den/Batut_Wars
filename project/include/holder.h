da05009b85596ee530da830958ff1be05243c73c
AlexanderK <alexander.klonov@mail.ru>
2020-12-06 15:46:55 +0300

    Added holders

--- project/include/holder.h
+++ project/include/holder.h
@@ -20,3 +20,26 @@ class Holder {
 };
 
 }  // namespace animation
+
+namespace fonts {
+   class Holder {
+    public:
+      Holder();
+      ~Holder() = default;
+      const sf::Font* get_texture(Id id) const;
+
+    private:
+      std::vector<sf::Texture> _resource_map;
+   }
+} // namspace fonts
+
+namespace textures {
+   class Holder {
+      public:
+      Holder();
+      ~Holder() = default;
+      const sf::Texture* get_texture(Id id) const;
+    private:
+      std::vector<sf::Texture> _resource_map;
+   }
+} // namespace textures
--- project/src/holder.cpp
+++ project/src/holder.cpp
@@ -6,6 +6,32 @@ Holder::Holder() {
     sf::Texture tmp;
     tmp.loadFromFile("project/media/MF_test.png");
     _resource_map.push_back(tmp);  // TODO(ANDY) дополнять
+}
+
+const sf::Texture* Holder::get_texture(Id id) const {
+    return &_resource_map[static_cast<size_t>(id)];
+}
+
+}  // namespace animation
+
+namespace fonts {
+
+Holder::Holder() {
+    sf::Font tmp;
+    tmp.loadFromFile("project/media/Sansation.ttf");
+    _resource_map.push_back(tmp);  // TODO(ANDY) дополнять
+}
+
+const sf::Texture* Holder::get_texture(Id id) const {
+    return &_resource_map[static_cast<size_t>(id)];
+}
+
+}  // fonts
+
+namespace textures {
+
+Holder::Holder() {
+    sf::Texture tmp;
     tmp.loadFromFile("project/media/space.jpg");
     _resource_map.push_back(tmp);
 }
@@ -14,4 +40,4 @@ const sf::Texture* Holder::get_texture(Id id) const {
     return &_resource_map[static_cast<size_t>(id)];
 }
 
-}  // namespace animation
+}  // textures
