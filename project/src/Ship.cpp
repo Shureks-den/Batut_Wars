64c559c6e4b1bf4346d4fc33f4f4f9c3c25cbb94
Tony Muzhts <muzhts.anton@gmail.com>
2020-12-24 01:21:56 +0300

    The latest version of bumping-building. BUGS + bad motion

--- project/src/Ship.cpp
+++ project/src/Ship.cpp
@@ -23,18 +23,44 @@ void Ship::collision(engine::MoveAble &other) {
   other.set_hp(other.get_hp() - 20);
   std::cout << "HP: " << this->get_hp() << std::endl;
 
+  // Just some physics
   engine::Vector V10 = this->get_engine_speed();
   engine::Vector V20 = other.get_engine_speed();
 
-  int a = V10.get_x() + V20.get_x();
-  int b = V10.get_y() + V20.get_y();
-  int c = V10.get_x() * V10.get_x() + V20.get_x() * V20.get_x();
-  int d = V10.get_y() * V10.get_y() + V20.get_y() * V20.get_y();
+  float a = V10.get_x() + V20.get_x();
+  float b = V10.get_y() + V20.get_y();
+  float c = V10.get_x() * V10.get_x() + V20.get_x() * V20.get_x();
+  float d = V10.get_y() * V10.get_y() + V20.get_y() * V20.get_y();
 
-  int V1x = (a + sqrt(2 * c - a * a)) / 2;
-  int V2x = (a - sqrt(2 * c - a * a)) / 2;
-  int V1y = (b + sqrt(2 * d - b * b)) / 2;
-  int V2y = (b - sqrt(2 * d - b * b)) / 2;
+  if (2 * c - a * a < 0 || 2 * d - b * b < 0) {
+    std::cout << 2 * c - a * a << "\t" << 2 * d - b * b << std::endl;
+  }
+
+  float V1x, V2x, V1y, V2y;
+  if (2 * c - a * a >= 0 && 2 * d - b * b >= 0) {
+    V1x = (a + sqrt(2 * c - a * a)) / 2;
+    V2x = (a - sqrt(2 * c - a * a)) / 2;
+    V1y = (b + sqrt(2 * d - b * b)) / 2;
+    V2y = (b - sqrt(2 * d - b * b)) / 2;
+  }
+  if (2 * c - a * a < 0 && 2 * d - b * b >= 0) {
+    V1x = V2x = 0;
+    V1y = (b + sqrt(2 * d - b * b)) / 2;
+    V2y = (b - sqrt(2 * d - b * b)) / 2;
+  }
+  if (2 * c - a * a >= 0 && 2 * d - b * b < 0) {
+    V1y = V2y = 0;
+    V1x = (a + sqrt(2 * c - a * a)) / 2;
+    V2x = (a - sqrt(2 * c - a * a)) / 2;
+  }
+  if (2 * c - a * a < 0 && 2 * d - b * b < 0) {
+    V1y = V2y = V1x = V2x = 0;
+  }
+
+  std::cout << V10.get_x() << "\t" << V10.get_y() << "\t//\t" << V20.get_x()
+            << "\t" << V20.get_y() << std::endl;
+  std::cout << V1x << "\t" << V1y << "\t//\t" << V2x << "\t" << V2y << std::endl
+            << std::endl;
 
   this->set_engine_speed(engine::Vector(V2x, V2y));
 
